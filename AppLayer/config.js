dcb77616f19f06e98813adc2d976e91eb6309c32
Wasim Pathan <wasim.pathan@synerzip.com>
2019-01-26 15:17:59 +0530

    Connection with remote DB

--- AppLayer/config.js
+++ AppLayer/config.js
@@ -3,10 +3,10 @@ var goviddo = goviddo || {};
 (function (goviddo) {
     goviddo.config = {
         port: 3000,
-        host: 'localhost',
-        user: 'root',
-        password: 'power123',
-        database: 'govid'
+        host: 'sql138.main-hosting.eu',
+        user: 'u450042391_govid',
+        password: 'Pratik@GoViddo',
+        database: 'u450042391_govid'
     };
 })(goviddo);
 
---
+++ AppLayer/config.local.js
@@ -0,0 +1,13 @@
+var goviddo = goviddo || {};
+
+(function (goviddo) {
+    goviddo.config = {
+        port: 3000,
+        host: 'localhost',
+        user: 'root',
+        password: 'power123',
+        database: 'govid'
+    };
+})(goviddo);
+
+module.exports = goviddo;
