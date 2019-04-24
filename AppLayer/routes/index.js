c22c38e38bd47062d27611416da990f1b9aeb768
Mac <=>
2019-04-24 19:02:22 +0530

    Update App

--- AppLayer/routes/index.js
+++ AppLayer/routes/index.js
@@ -176,7 +176,7 @@ app.get(
             "is_run_mode":"true",
             "name":"GoViddo AndroidAppUpdater",
             "uri_current":"blockchainvideoapp.com.goviddo.goviddo",
-            "version_code_current":3,
+            "version_code_current":5,
             "version_code_min":1,
             "update_info":"On version 1.3 we added App Update Notification",
             "update_date":"24/04/2019"
