48c1c02b77de6a29659989cc53fc261d1dbb56aa
Mac <simtechnospace27@gmail.com>
2019-06-26 15:04:40 +0530

    reg

--- AppLayer/routes/index.js
+++ AppLayer/routes/index.js
@@ -2067,13 +2067,13 @@ app.get(
                                         var userId = mresn[0].user_id;
 
 
-
                                         let queryInsertTransactions = "INSERT INTO `video_transactions`(`transaction_amount`, `transaction_user_id`, `transaction_memo`, `transaction_from`) VALUES ('0.01 GOV','"+userId+"','For Registration of New User','hellogoviddo')";
 
                                         db.query(queryInsertTransactions, function(mresr, mresultmm){
 
-                                            console.log(resp);
-                                        return res.status(200).send(resp);
+                                            var d = res.status(200).send(resp);
+                                            console.log(d);
+                                        return d;
 
                                         });
 
