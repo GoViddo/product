e029c95f551b8a298427fa2de9949dee207ce0d2
Mac <simtechnospace27@gmail.com>
2019-06-26 15:40:07 +0530

    reg

--- AppLayer/routes/index.js
+++ AppLayer/routes/index.js
@@ -1896,7 +1896,7 @@ app.get(
 
                 console.log(result.length);
 
-            if (result.length > 1) {
+            if (result.length) {
                 resp.message = "User with this email already exists";
                 return res.status(200).send(resp);
             } else {
