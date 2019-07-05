42d1686418cb2a6d76864804cb70371857142519
Mac <simtechnospace27@gmail.com>
2019-07-05 22:29:13 +0530

    Cordova API

--- AppLayer/app.js
+++ AppLayer/app.js
@@ -53,7 +53,12 @@ handleDisconnect();
 app.set('port', process.env.port || port); // set express to use this port
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
-app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // routes for the app
 app.get('/config', getConfig);
